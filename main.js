40a8739b4f256dcb1e7e004d3b7870a0fb392017
crudybagger <sarangnagpal38@gmail.com>
2023-08-05 05:29:11 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,76 @@
+var audio = new Audio('cat-meow.mp3');
+
+
+const startButton = document.getElementById('start-button');
+const modal = document.getElementById('modal');
+const modalBoundingRect = modal.getBoundingClientRect();
+const distance = (x, y, element) => {
+    const rect = element.getBoundingClientRect();
+    return Math.sqrt(Math.pow(x - rect.left, 2) + Math.pow(y - rect.top, 2));
+}
+const linearInterpolation = (t, p0, p1) => {
+    return p0 + (p1 - p0) * t;
+}
+const quadraticBezier = (t, p0, p1, p2) => {
+    return linearInterpolation(t, linearInterpolation(t, p0, p1), linearInterpolation(t, p1, p2));
+}
+const cubicBezier = (t, p0, p1, p2, p3) => {
+    return linearInterpolation(t, quadraticBezier(t, p0, p1, p2), quadraticBezier(t, p1, p2, p3));
+}
+const scale = (t) => {
+    // cubic-bezier(.5,.5,1,.5)
+    t =  t / Math.sqrt(Math.pow(window.innerWidth, 2) + Math.pow(window.innerHeight,2));
+    return cubicBezier(1-t, 0, 0, .5, 1);
+}
+
+
+const cat = document.createElement('div');
+cat.id = 'cat';
+cat.style.width = '50px';
+cat.style.height = '50px';
+cat.style.position = 'absolute';
+cat.style.top = 0;
+cat.style.left = 0;
+document.body.appendChild(cat);
+
+const intersects = (rect1, rect2) => {
+    return rect1.left < rect2.right && rect1.right > rect2.left && rect1.top < rect2.bottom && rect1.bottom > rect2.top;
+}
+
+const generateRandomPosition = () => {
+    cat.style.top = Math.random() * (window.innerHeight - 50);
+    cat.style.left = Math.random() * (window.innerWidth - 50);
+    while(intersects(cat.getBoundingClientRect(), modalBoundingRect)) {
+        generateRandomPosition();
+    }
+}
+
+
+document.addEventListener('mousemove', (event) => {
+    const x = event.clientX;
+    const y = event.clientY;
+    var val = scale(distance(x, y, cat));
+    audio.volume = val;
+})
+
+cat.addEventListener('click', async () => {
+    audio.pause();
+    audio.currentTime = 0;
+    audio.loop = false;
+    audio.playbackRate = 1;
+    await audio.play();
+    modal.style.display = 'block';
+    cat.style.backgroundImage = "url('cat.png')";
+})
+startButton.addEventListener('click', () => {
+    modal.style.display = 'none';
+    cat.style.backgroundColor = '#f2f2f2';
+    cat.style.backgroundImage = 'none'
+    
+    cat.style.backgroundColor = 'red';
+    generateRandomPosition();
+    audio.loop = true;
+    audio.currentTime = 0;
+    audio.playbackRate = 12;
+    audio.play();
+});
